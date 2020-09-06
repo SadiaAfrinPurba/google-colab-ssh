bf191689aa9ab5942add186ae59eb75f82eeeff0
SadiaAfrinPurba <purba0101@gmail.com>
2020-09-07 01:06:42 +0600

    Rearrange commands

--- run.py
+++ run.py
@@ -14,6 +14,8 @@ def connect_ngork():
 
 
 if __name__ == '__main__':
+    url = connect_ngork()
+
     print('Installing code-server for Google Colab')
     code_server_install_command = f'sh scripts/code_server_install.sh'
     call(code_server_install_command.split())
@@ -23,8 +25,5 @@ if __name__ == '__main__':
     clone_github_private_repo(email, name)
     print('Add SSH key to your GitHub account')
 
-    url = connect_ngork()
-    code_server_start_command = f'nohup code-server --port 9000 --auth none'
-    call(code_server_start_command.split())
     print('Now go to below address to use code-server')
     print(url)
