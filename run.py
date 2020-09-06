afdf95abc3dca42cc68a80027efae1897ccf2690
SadiaAfrinPurba <purba0101@gmail.com>
2020-09-07 02:53:13 +0600

    Use subprocess.run() instead of subprocess.call()

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from subprocess import call
+import subprocess
 
 from pyngrok import ngrok
 
 
 def clone_github_private_repo(email: str, name: str) -> None:
     private_repo_commands = f'sh scripts/private_repo_clone.sh {email} {name}'
-    call(private_repo_commands.split())
+    subprocess.call(private_repo_commands.split())
 
 
 def connect_ngork():
@@ -14,19 +14,19 @@ def connect_ngork():
 
 
 if __name__ == '__main__':
+    url = connect_ngork()
+
     email = input('Give email address for git config settings: ')
     name = input('Give name for git config settings: ')
     clone_github_private_repo(email, name)
     print('Add SSH key to your GitHub account')
 
-    print('Installing code-server for Google Colab')
-    code_server_install_command = f'sh scripts/code_server_install.sh'
-    call(code_server_install_command.split())
-
-    url = connect_ngork()
-
-    code_server_run_command = 'nohup code-server --port 9000 --auth none &'
-    call(code_server_run_command.split())
-
     print('Now go to below address to use code-server')
     print(url)
+
+    print('Installing code-server for Google Colab')
+    code_server_install_command = f'sh scripts/code_server_install.sh'
+    subprocess.run(code_server_install_command.split())
+
+    # code_server_run_command = 'nohup code-server --port 9000 --auth none &'
+    # subprocess.run(code_server_run_command.split())
--- scripts/code_server_install.sh
+++ scripts/code_server_install.sh
@@ -2,3 +2,4 @@
 
 curl -fsSL https://code-server.dev/install.sh | sh
 echo "Code server install done!"
+nohup code-server --port 9000 --auth none &
