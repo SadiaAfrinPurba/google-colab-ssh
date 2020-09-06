b97ff9a04485c424aa1fb34d3aeaa275af2f7d98
SadiaAfrinPurba <purba0101@gmail.com>
2020-09-07 01:56:43 +0600

    Follow the exact procedure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='colab-ssh-key',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/SadiaAfrinPurba/google-colab-ssh',
+    license='MIT',
+    author='afrin',
+    author_email='purba0101@gmail.com',
+    description='Generate SSH key and setup Git config settings in Google Colab. After that, private Github repositories can be cloned from Google Colab. Git pull, commit and push functionality will be enable for private repositories.'
+)
